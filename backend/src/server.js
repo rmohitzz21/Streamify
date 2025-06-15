01685ac03144a0b2877ba5a01ae7098ff5de90f4
rmohitzz21 <rmohit21264@gmail.com>
2025-06-15 12:02:17 +0530

    To Delpoy

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,10 +6,13 @@ import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 import chatRoutes from './routes/chat.route.js';
 import {connectDB} from './lib/db.js'
+import path from "path";
+
 
 const app = express();
 const PORT  = process.env.PORT
 
+const __dirname = path.resolve();
 
 // It is used to allow the frontend to communicate with the backend
 app.use(cors({
@@ -23,6 +26,14 @@ app.use(cookieParser()); // Middleware to parse cookies
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/chat', chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, ()=> {
     console.log(`Server is Running on ${PORT} !`); 
